5856a164d4767a017db63d755b7780a13aaa6393
Stephen Trewick <stephent818@gmail.com>
2019-06-29 21:22:55 -0700

    css to the navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import NavBar from "./NavBar";
 
 function App() {
+  var appStyles = {
+    marginLeft: '50px',
+    marginTop: '15px',
+  }
   return(
-    <div>
+    <div style={appStyles}>
       <NavBar/>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 function NavBar() {
+  var styling = {
+    border: '3px solid lightgray',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1px',
+    width: '30%',
+    letterSpacing: '0.6px',
+    placeItems: 'center',
+    fontSize: '15px',
+    fontFamily: 'Montserrat',
+  }
   return (
-    <div>
+    <div style = {styling}>
       <h3>Home</h3>
       <h3>Notifications</h3>
       <h3>Messages</h3>
+      <hr></hr>
     </div>
   );
 }
